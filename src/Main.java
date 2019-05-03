fec61715825de679cfbc0687af7ed7e20f0a0eeb
Sokratis Papadopoulos <sokpao@gmail.com>
2019-05-03 21:42:35 +0200

    processed all CSV (todo: published_in [pages attr])

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class Main {
 
-    private static final String inputFileName = "SDMlab3.owl";
+    private static final String inputFileName = "research.owl";
     private static final String SOURCE = "http://www.semanticweb.org/saradiaz/ontologies/2019/3/SDMlab3";
     private static final String NS = SOURCE + "#";
 
@@ -35,30 +35,314 @@ public class Main {
         virtGraph.clear();
         virtModel = new VirtModel(virtGraph);
 
-        System.out.println("Importing papers...");
-        processPapers();
-        System.out.println("Papers imported.");
+        long start = System.currentTimeMillis();
 
-        System.out.println("Importing keywords...");
-        processKeywords();
-        System.out.println("Keywords imported.");
+        System.out.print("Importing papers...");
+        processPapers(); System.out.print("Done.\n");
 
-        System.out.println("Importing related (keyword,paper)...");
-        processRelated();
-        System.out.println("Related imported.");
+        System.out.print("Importing cited_by (paper, paper)...");
+        processCitedBy(); System.out.print("Done.\n");
 
-        System.out.println("Importing authors...");
-        processAuthors();
-        System.out.println("Authors imported.");
+        System.out.print("Importing keywords...");
+        processKeywords(); System.out.print("Done.\n");
 
-        System.out.println("Importing writes and main_author...");
-        processWrites();
-        System.out.println("Writes and main_author imported.");
+        System.out.print("Importing related (keyword,paper)...");
+        processRelated(); System.out.print("Done.\n");
 
-        System.out.println("Importing reviews...");
-        processReviews();
-        System.out.println("Reviews imported.");
+        System.out.print("Importing authors...");
+        processAuthors(); System.out.print("Done.\n");
 
+        System.out.print("Importing writes and main_author...");
+        processWrites(); System.out.print("Done.\n");
+
+        System.out.print("Importing reviews...");
+        processReviews(); System.out.print("Done.\n");
+
+        System.out.print("Importing journals...");
+        processJournals(); System.out.print("Done.\n");
+
+        System.out.print("Importing conferences...");
+        processConferences(); System.out.print("Done.\n");
+
+        System.out.print("Importing editions...");
+        processEditions(); System.out.print("Done.\n");
+
+        System.out.print("Importing belongs (edition, conference)...");
+        processBelongs(); System.out.print("Done.\n");
+
+        System.out.print("Importing published_in (paper, edition)...");
+        processPaperPublishedInEdition(); System.out.print("Done.\n");
+
+        System.out.print("Importing volumes...");
+        processVolumes(); System.out.print("Done.\n");
+
+        System.out.print("Importing published_in (paper, volume)...");
+        processPaperPublishedInVolume(); System.out.print("Done.\n");
+
+        System.out.print("Importing part_of (volume, journal)...");
+        processVolumePartOfJournal(); System.out.print("Done.\n");
+
+        double total = (1.0*System.currentTimeMillis() - start)/60000;
+        System.out.println("\nTotal time: " + total + " minutes");
+
+    }
+
+    //TODO: adding pages on published_in (both)
+
+    /**
+     * Reads belogns (edition, conference) from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processVolumePartOfJournal() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/volumeBelongsJournal.csv"));
+        ObjectProperty part_of = base.getObjectProperty(NS + "part_of");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long volume_id = Long.parseLong(tokens[0]);
+            long journal_id = Long.parseLong(tokens[1]);
+
+            Resource volume = base.getResource(NS + volume_id);
+            Resource journal = base.getResource(NS + journal_id);
+
+            statements.add(ResourceFactory.createStatement(volume, part_of, journal));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads edition_published_in (paper, edition) from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processPaperPublishedInVolume() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/volume_published_in.csv"));
+        ObjectProperty published_in = base.getObjectProperty(NS + "published_in");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long paper_id = Long.parseLong(tokens[0]);
+            long volume_id = Long.parseLong(tokens[1]);
+
+            Resource paper = base.getResource(NS + paper_id);
+            Resource volume = base.getResource(NS + volume_id);
+
+            statements.add(ResourceFactory.createStatement(paper, published_in, volume));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads Editions from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processVolumes() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/volume.csv"));
+        OntClass cls = base.getOntClass(NS + "Volume");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long volume_id = Long.parseLong(tokens[0]);
+            String vol = tokens[1];
+            String title = tokens[2];
+
+            // create Volume properties
+            Individual volume = cls.createIndividual(NS + volume_id);
+
+            DatatypeProperty has_title = base.getDatatypeProperty(NS + "title");
+            Literal title_value = base.createTypedLiteral(title, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(volume, has_title, title_value));
+
+            DatatypeProperty has_vol = base.getDatatypeProperty(NS + "vol");
+            Literal vol_value = base.createTypedLiteral(vol, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(volume, has_vol, vol_value));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads edition_published_in (paper, edition) from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processPaperPublishedInEdition() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/edition_published_in.csv"));
+        ObjectProperty published_in = base.getObjectProperty(NS + "published_in");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long paper_id = Long.parseLong(tokens[0]);
+            long edition_id = Long.parseLong(tokens[1]);
+
+            Resource paper = base.getResource(NS + paper_id);
+            Resource edition = base.getResource(NS + edition_id);
+
+            statements.add(ResourceFactory.createStatement(paper, published_in, edition));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads belogns (edition, conference) from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processBelongs() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/belongs.csv"));
+        ObjectProperty belongs_to = base.getObjectProperty(NS + "belongs_to");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long edition_id = Long.parseLong(tokens[0]);
+            long conference_id = Long.parseLong(tokens[1]);
+
+            Resource edition = base.getResource(NS + edition_id);
+            Resource conference = base.getResource(NS + conference_id);
+
+            statements.add(ResourceFactory.createStatement(edition, belongs_to, conference));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads Editions from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processEditions() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/edition.csv"));
+        OntClass cls = base.getOntClass(NS + "Edition");
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long edition_id = Long.parseLong(tokens[0]);
+            String title = tokens[1];
+            String venue = tokens[2];
+            String city = tokens[3]; //skipping 4: period
+            String year = tokens[5];
+
+            // create Edition properties
+            Individual edition = cls.createIndividual(NS + edition_id);
+
+            DatatypeProperty has_title = base.getDatatypeProperty(NS + "title");
+            Literal title_value = base.createTypedLiteral(title, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(edition, has_title, title_value));
+
+            DatatypeProperty has_venue = base.getDatatypeProperty(NS + "venue");
+            Literal venue_value = base.createTypedLiteral(venue, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(edition, has_venue, venue_value));
+
+            DatatypeProperty has_city = base.getDatatypeProperty(NS + "city");
+            Literal city_value = base.createTypedLiteral(city, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(edition, has_city, city_value));
+
+            DatatypeProperty has_year = base.getDatatypeProperty(NS + "year");
+            Literal year_value = base.createTypedLiteral(year, XSDDatatype.XSDint);
+            statements.add(base.createStatement(edition, has_year, year_value));
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads cited_by (paper, paper) from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processCitedBy() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/cited_by_year.csv"));
+        ObjectProperty citedBy = base.getObjectProperty(NS + "cited_by");
+
+        String line = br.readLine(); //remove header: Author_ID - Paper_ID
+        while ((line = br.readLine()) != null) {
+            String[] tokens = line.split(";");
+            long paper1_id = Long.parseLong(tokens[0]);
+            long paper2_id = Long.parseLong(tokens[1]);
+
+            Resource originPaper = base.getResource(NS + paper1_id);
+            Resource citedByPaper = base.getResource(NS + paper2_id);
+
+            statements.add(ResourceFactory.createStatement(originPaper, citedBy, citedByPaper));
+        }
+
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads conferences from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processConferences() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/conferences.csv"));
+        OntClass cls = base.getOntClass(NS + "Conference");
+        List<String> conf_types = getSubclasses(cls);
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            int random = (int) (Math.random() * conf_types.size());
+            String[] tokens = line.split(";");
+            long journal_id = Long.parseLong(tokens[0]);
+            String title = tokens[1];
+            String conf_type = conf_types.get(random);
+
+            // create Conference properties
+            Individual conference = base.getOntClass(NS + conf_type).createIndividual(NS + journal_id);
+            DatatypeProperty has_title = base.getDatatypeProperty(NS + "title");
+            Literal title_value = base.createTypedLiteral(title, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(conference, has_title, title_value));
+
+        }
+        virtModel.add(statements);
+    }
+
+    /**
+     * Reads journals from csv file and inserts triplet instances into virtuoso.
+     *
+     * @throws IOException
+     */
+    private static void processJournals() throws IOException {
+        List<Statement> statements = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader("input/journal.csv"));
+        OntClass cls = base.getOntClass(NS + "Journal");
+        List<String> journal_types = getSubclasses(cls);
+
+        String line = br.readLine(); //remove header
+        while ((line = br.readLine()) != null) {
+            int random = (int) (Math.random() * journal_types.size());
+            String[] tokens = line.split(";");
+            long journal_id = Long.parseLong(tokens[0]);
+            String title = tokens[1];
+            String journal_type = journal_types.get(random);
+
+            // create Journal properties
+            Individual journal = base.getOntClass(NS + journal_type).createIndividual(NS + journal_id);
+            DatatypeProperty has_title = base.getDatatypeProperty(NS + "title");
+            Literal title_value = base.createTypedLiteral(title, XSDDatatype.XSDstring);
+            statements.add(base.createStatement(journal, has_title, title_value));
+
+        }
+        virtModel.add(statements);
     }
 
     /**
